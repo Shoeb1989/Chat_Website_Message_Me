e8a42d5e57f625a913390c1a3756d1d087bbcd56
Shoeb1989 <psikder09@gmail.com>
2024-06-29 17:32:10 +0600

    update

--- me.js
+++ me.js
@@ -1,4 +1,21 @@
-function milesToKillo(miles){
-    const killometer = miles*1.609;
-    return killometer;
+// function milesToKillo(miles){
+//     const killometer = miles*1.609;
+//     return killometer;
+// }
+
+function isEven(number){
+    const remainder = number % 2;
+    if (remainder === 0){
+        return true;
+    }
+    else {
+        return false;
+    } 
 }
+
+const mynumber = isEven(303);
+console.log(mynumber);
+const mynumberr = isEven(1200);
+console.log(mynumberr);
+
+
