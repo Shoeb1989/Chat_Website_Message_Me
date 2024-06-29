3ed6de2d87ce334c07e00099ca6f00e053cdba21
Shoeb1989 <psikder09@gmail.com>
2024-06-29 17:46:14 +0600

    update

--- me.js
+++ me.js
@@ -1,21 +1,29 @@
-// function milesToKillo(miles){
-//     const killometer = miles*1.609;
-//     return killometer;
-// }
+// // function milesToKillo(miles){
+// //     const killometer = miles*1.609;
+// //     return killometer;
+// // }
 
-function isEven(number){
-    const remainder = number % 2;
+// function isEven(number){
+//     const remainder = number % 2;
+//     if (remainder === 0){
+//         return true;
+//     }
+//     else {
+//         return false;
+
+// const mynumber = isEven(303);
+// console.log(mynumber);
+// const mynumberr = isEven(1200);
+// console.log(mynumberr);
+
+function isleapYear (year){
+    const remainder = year % 4;
     if (remainder === 0){
-        return true;
+        console.log('your year is leap year');
     }
     else {
-        return false;
-    } 
+        console.log('your year is not leap year');
+    }
 }
 
-const mynumber = isEven(303);
-console.log(mynumber);
-const mynumberr = isEven(1200);
-console.log(mynumberr);
-
-
+isleapYear(1950);
