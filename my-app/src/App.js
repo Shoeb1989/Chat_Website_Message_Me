8249496dbf8e61294c4f8832e4c469639745ae26
Shoeb1989 <psikder09@gmail.com>
2024-06-29 21:02:40 +0600

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,4 +5,45 @@ import {
     Switch,
     Redirect
 }   from 'react-router-dom';
-import Home from './pages/home/home'
+import home from './pages/home/home';
+import chat from './pages/Chat/chat';
+import profile from './pages/profile/profile';
+import signup from './pages/signup/signup';
+import Login from './pages/Login/login';
+import firebase from './Services/firebase';
+import {toast , ToastContainer} from 'react-toastify';
+import Loading from 'react-loading';
+
+class App extends Component{
+
+    showToast = (type, message) => {
+      switch(type){
+        case 0:
+        toast.warning(message)
+        break;
+        case 1:
+            toast.success(message)
+            default :
+              break;
+      }  
+    }
+    constructor(){
+        super();
+        this.state = {
+            authenticated: false,
+            Loading: true
+        };
+    }
+     componentDidMount(){
+        firebase.auth().onAuthstatechanged(user => {
+  
+         if(user){
+            this.setState()
+         }   
+        })
+     }
+     render(){
+        return()
+     }
+
+}
