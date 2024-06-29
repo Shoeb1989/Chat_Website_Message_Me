d2661398caa80660c7bd5ca6d5c8be41014848e8
Shoeb1989 <psikder09@gmail.com>
2024-06-29 20:44:56 +0600

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,1 +1,8 @@
 import react, { Component } from 'react';
+import {
+    Route,
+    BrowserRouter as Router,
+    Switch,
+    Redirect
+}   from 'react-router-dom';
+import Home from './pages/home/home'
